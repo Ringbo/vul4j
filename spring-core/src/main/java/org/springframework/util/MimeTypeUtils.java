3ce066ebf66a9be9185614e01bb4d97fa2ffeab9
Ringbo <874184975@qq.com>
2025-06-13 12:43:06 +0800

    Update MimeTypeUtils.java

--- spring-core/src/main/java/org/springframework/util/MimeTypeUtils.java
+++ spring-core/src/main/java/org/springframework/util/MimeTypeUtils.java
@@ -27,7 +27,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
+import java.security.SecureRandom;
 import org.springframework.util.MimeType.SpecificityComparator;
 
 /**
